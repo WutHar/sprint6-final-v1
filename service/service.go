e26597542ff37778709192a8a7512d8d54c92080
WutHar <vikingcrewnord@gmail.com>
2025-05-16 03:46:18 +0400

    удалил константу переделал service.go

--- service/service.go
+++ service/service.go
@@ -1,29 +1,41 @@
 package service
 
 import (
-	"strings"
-
 	"github.com/WutHar/sprint6-final/pkg/morse"
 )
 
-// DetectAndConvert определяет тип входящей строки (текст или код Морзе) и конвертирует её.
 func DetectAndConvert(data string) (string, error) {
-
 	isMorse := true
 	for _, char := range data {
-		if !strings.Contains(morse.Alphabet, strings.ToUpper(string(char))) && char != ' ' && char != '.' && char != '-' {
+		if char != '.' && char != '-' && char != ' ' {
 			isMorse = false
 			break
 		}
 	}
 
 	if isMorse {
-
 		text := morse.ToText(data)
 		return text, nil
 	} else {
-
 		morseCode := morse.ToMorse(data)
 		return morseCode, nil
 	}
+}
+
+type Converter struct {
+	morseConverter morse.Converter
+}
+
+func NewConverter() *Converter {
+	return &Converter{
+		morseConverter: morse.DefaultConverter,
+	}
+}
+
+func (c *Converter) TextToMorse(text string) string {
+	return c.morseConverter.ToMorse(text)
+}
+
+func (c *Converter) MorseToText(morseCode string) string {
+	return c.morseConverter.ToText(morseCode)
 }
