89bf74fd6c7c636c4a71791692e7a84605e53783
WutHar <vikingcrewnord@gmail.com>
2025-05-17 02:46:24 +0400

    обновил функцию DetectAndConvert

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"log"
 	"strings"
 	"unicode"
 
@@ -8,23 +9,23 @@ import (
 )
 
 func DetectAndConvert(data string) (string, error) {
+	log.Printf("Входящие данные в DetectAndConvert: %q", data)
 
-	textFromMorse := morse.ToText(data)
-	textFromMorse = strings.TrimSpace(textFromMorse)
-
-	isLikelyMorse := false
-	for _, r := range textFromMorse {
-		if unicode.IsLetter(r) || unicode.IsDigit(r) {
-			isLikelyMorse = true
+	isMorse := true
+	for _, char := range data {
+		if !unicode.Is(unicode.Dash, char) && char != '.' && char != ' ' {
+			isMorse = false
 			break
 		}
 	}
 
-	if isLikelyMorse {
-		return textFromMorse, nil
+	if isMorse && strings.ContainsAny(data, ".-") { // Дополнительная проверка на наличие хотя бы одного символа Морзе
+		text := morse.ToText(data)
+		log.Printf("Определено как код Морзе, результат ToText: %q", text)
+		return text, nil
 	} else {
-
 		morseCode := morse.ToMorse(data)
+		log.Printf("Определено как обычный текст, результат ToMorse: %q", morseCode)
 		return morseCode, nil
 	}
 }
